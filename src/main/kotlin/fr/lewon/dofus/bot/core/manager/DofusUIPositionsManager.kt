90ca9d43f566199666ea2437710cbe732f30c47b
Lewon <Victor.Lewon@amersports.com>
2021-11-02 20:39:14 +0100

    Small refactoring

--- src/main/kotlin/fr/lewon/dofus/bot/core/manager/DofusUIPositionsManager.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/core/manager/DofusUIPositionsManager.kt
@@ -1,65 +1,27 @@
 package fr.lewon.dofus.bot.core.manager
 
 import fr.lewon.dofus.bot.core.manager.dat.DatUtil
+import fr.lewon.dofus.bot.core.manager.ui.UIPoint
 
 object DofusUIPositionsManager : VldbManager {
 
-    private const val INFO_TYPE_POSITION = "pos"
-    private const val INFO_TYPE_SIZE = "size"
-
     const val CONTEXT_DEFAULT = "default"
     const val CONTEXT_FIGHT = "fight"
-
-    private const val CTR_HUNT = "ctr_hunt"
-    private const val CTR_MAIN = "ctr_main"
-    private const val MAIN_CTR = "mainCtr"
 
     override fun initManager() {
         // Nothing
     }
 
+    fun getZaapSelectionUiPosition(): UIPoint? {
+        return getUIPoint("zaapSelection", "pos", "window316", CONTEXT_DEFAULT)
+    }
+
     fun getBannerUiPosition(context: String = CONTEXT_DEFAULT): UIPoint? {
-        return getUIPoint("banner", INFO_TYPE_POSITION, MAIN_CTR, context)
-    }
-
-    fun getBannerMapUiPosition(): UIPoint? {
-        return getUIPoint("bannerMap", INFO_TYPE_POSITION, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getBannerMapUiSize(): UIPoint? {
-        return getUIPoint("bannerMap", INFO_TYPE_SIZE, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getBannerMenuUiPosition(context: String = CONTEXT_DEFAULT): UIPoint? {
-        return getUIPoint("bannerMenu", INFO_TYPE_POSITION, MAIN_CTR, context)
-    }
-
-    fun getBannerMenuUiSize(): UIPoint? {
-        return getUIPoint("bannerMenu", INFO_TYPE_SIZE, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getBuffUiPosition(): UIPoint? {
-        return getUIPoint("buffUi", INFO_TYPE_POSITION, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getChatUiPosition(context: String = CONTEXT_DEFAULT): UIPoint? {
-        return getUIPoint("chat", INFO_TYPE_POSITION, MAIN_CTR, context)
-    }
-
-    fun getChatUiSize(): UIPoint? {
-        return getUIPoint("chat", INFO_TYPE_SIZE, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getExternalActionBarUiPosition(actionBarId: Int): UIPoint? {
-        return getUIPoint("externalActionBar_$actionBarId", INFO_TYPE_POSITION, MAIN_CTR, CONTEXT_DEFAULT)
-    }
-
-    fun getQuestListUiPosition(): UIPoint? {
-        return getUIPoint("questList", INFO_TYPE_POSITION, CTR_MAIN, CONTEXT_DEFAULT)
+        return getUIPoint("banner", "pos", "mainCtr", context)
     }
 
     fun getTreasureHuntUiPosition(): UIPoint? {
-        return getUIPoint("treasureHunt", INFO_TYPE_POSITION, CTR_HUNT, CONTEXT_DEFAULT)
+        return getUIPoint("treasureHunt", "pos", "ctr_hunt", CONTEXT_DEFAULT)
     }
 
     private fun getUIPoint(uiItemKey: String, infoType: String, ctr: String, context: String): UIPoint? {
@@ -71,8 +33,6 @@ object DofusUIPositionsManager : VldbManager {
     private fun buildKey(uiItemKey: String, infoType: String, ctr: String, context: String): String {
         return "$uiItemKey##$infoType##$ctr##$context"
     }
-
-    class UIPoint(var x: Float = 0f, var y: Float = 0f)
 
     private class DofusUIPointByKey : HashMap<String, UIPoint>()
 }
---
+++ src/main/kotlin/fr/lewon/dofus/bot/core/manager/ui/UIPoint.kt
@@ -0,0 +1,3 @@
+package fr.lewon.dofus.bot.core.manager.ui
+
+class UIPoint(var x: Float = 0f, var y: Float = 0f)
